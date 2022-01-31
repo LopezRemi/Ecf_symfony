8ea2554e580664b91473571808ccf43e3817ca9f
Anger romain <anger.romain@gmail.com>
2022-01-31 12:09:03 +0100

    function create et delete terminer

---
+++ ecf_symfony.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 31 jan. 2022 à 10:01
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ecf_symfony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `summary` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `release_date` date NOT NULL,
+  `category` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `book_condition` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `editor` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `summary`, `release_date`, `category`, `book_condition`, `editor`) VALUES
+(1, 'l\'assomoir', 'jean moulin', 'un livre qui assome', '2022-01-19', 'roman', 'bonne', 'grasset');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20220131082208', '2022-01-31 09:23:07', 29);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -3,10 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Books;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\DBAL\Types\BooleanType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BooksController extends AbstractController
 {
@@ -22,7 +28,6 @@ class BooksController extends AbstractController
     #[Route('/books/listing', name: 'books_listing')]
     public function booksListing(ManagerRegistry $doctrine): Response
     {
-
         $books = $doctrine->getRepository(Books::class)->findAll();
 
         return $this->render('books/listing.html.twig', [
@@ -30,5 +35,107 @@ class BooksController extends AbstractController
         ]);
     }
 
+    #[Route('/book/create', name: 'book_create')]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
+    {
+        // mise en place du gestionnaire de BDD :
+        $entityManager = $doctrine->getManager();
+        // cree l'objet book$book et initialise les datas
+        $book = new Books();
+        $book->setStatus(true);
+        $form = $this->createFormBuilder($book)
+        ->add('title', TextType::class, [
+            'label' => 'Titre du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ]
+         ])
+        ->add('author', TextType::class, [
+            'label' => 'Auteur du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ]
+        ])
+        ->add('release_date', DateType::class, [
+            'label' => 'Date de parution du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ]
+        ])
+        ->add('summary', TextType::class, [
+            'label' => 'Résumé du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ]
+        ])
+        ->add('category', TextType::class, [
+            'label' => 'Genre du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ]
+        ])
+        ->add('editor', TextType::class, [
+            'label' => 'Editeur du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ]
+        ])
+        ->add('status', ChoiceType::class, [
+            'label' => 'Disponibilité du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ],
+            'choices' => [
+                'Emprunte' => '0',  // 0 quand emprunter
+                'Disponible' => '1', // 1 quand disponible
+            ],
+        ])
+        ->add('book_condition', ChoiceType::class, [
+            'label' => 'Etat du livre',
+            'attr' => [
+                'class' => 'form-control mb-4'
+            ],
+            'choices' => [
+                'bon' => 'Bon état',
+                'passable' => 'Etat passable',
+                'mauvais' => 'Mauvais état',
+            ],
+        ])
+        ->add('save', SubmitType::class, [
+            'label' => 'Créé le livre',
+            'attr' => [
+                'class' => 'btn btn-primary'
+                ]])
+        ->getForm();
+    
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $entityManager->persist($book);
+            $entityManager->flush();
+            $this->addFlash('success', 'Livre ajoutée avec succes');
+
+            return $this->redirectToRoute('books_listing');
+        }
+
+        return $this->renderForm('books/create.html.twig', [
+        'form' => $form,
+    ]);
+    }
+
+    #[Route('/books/delete/{id}', name: 'book_delete')]
+    public function remove(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $book = $entityManager->getRepository(Books::class)->find($id);
+        
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        $this->addFlash('danger', 'Livre Supprimé avec succes');
+
+        return $this->redirectToRoute('books_listing');
+    }
 }
